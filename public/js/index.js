df4ad3734ea61832820f17ea4215a95df589ee2b
Manas Gabani <manasdineshgabani@gmail.com>
2019-02-08 15:44:44 +0530

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io(); // as we have loaded the socket.io.js ... it request server to establish a web socket and keep the connection open
+socket.on('connect',function(){
+	console.log('Connected to server');
+
+	// socket.emit('createEmail',{
+	// 	to: 'andrew@example.com',
+	// 	text: 'this is my text'
+	// });
+
+	socket.emit('createMessage',{
+		from: 'Julian',
+		text: 'Hola!!'
+	});
+});
+
+socket.on('disconnect',function(){
+	console.log('Disconnected from server');
+});
+
+// socket.on('newEmail',function(email) {
+// 	console.log('New Email',email);
+// });
+
+socket.on('newMessage',function(msg){
+	console.log('New Message:',msg);
+})
+
--- server/server.js
+++ server/server.js
@@ -17,6 +17,26 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('New user connected');
 
+	// socket.emit('newEmail',{
+	// 	from : 'mike@example.com',
+	// 	text : 'hello there!',
+	// 	createdAt: 123
+	// });
+
+	// socket.on('createEmail',(email)=>{
+	// 	console.log('createEmail',email);
+	// });
+
+	socket.emit('newMessage',{
+		to: 'Julian',
+		text: 'There you go',
+		createdAt: 123123
+	});
+
+	socket.on('createMessage',(msg)=>{
+		console.log('Create Message',msg);
+	});
+
 	socket.on('disconnect',()=>{
 		console.log('User was disconnected');
 	})
